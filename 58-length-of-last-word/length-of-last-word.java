83666e04983e3ba547d0d62533a11cb4d1a419cc
Abdulkadir Akcan <164941095+kadirakcn00@users.noreply.github.com>
2025-10-04 14:17:40 +0300

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.12%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+      s = s.trim();
+      int lastSpace = s.lastIndexOf(" ");
+      return s.length()-lastSpace-1;
+        
+    }
+}
